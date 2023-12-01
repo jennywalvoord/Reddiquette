2e9ce738dfadcd2a3615634cdbbefd1b23fc469d
Jenny Walvoord <jennywalvoord@gmail.com>
2023-12-01 14:48:03 -0500

    working version

--- vue/src/components/SideBar.vue
+++ vue/src/components/SideBar.vue
@@ -2,8 +2,8 @@
     <div>
         <button class="login"></button>
         <h3>Active Forums</h3>
-        <ul v-for="forum in forums" v-bind:key="id">
-            <li></li>
+        <ul v-for="forum in forums" v-bind:key="forum.id">
+            <li v-bind="forums"></li>
         </ul>
     </div>
 </template>
