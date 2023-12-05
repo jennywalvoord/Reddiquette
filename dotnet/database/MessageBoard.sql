b1665a8db36eb3c47c9ef977b7257ddc99f4567c
Mac Reske <richard.m.reske@Gmail.com>
2023-12-05 12:44:17 -0500

    updated tables

--- dotnet/database/MessageBoard.sql
+++ dotnet/database/MessageBoard.sql
@@ -20,18 +20,28 @@ CREATE TABLE users (
 	username varchar(50) NOT NULL,
 	password_hash varchar(200) NOT NULL,
 	salt varchar(200) NOT NULL,
-	user_role varchar(50) NOT NULL
+	user_role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
-)
+);
+
+
+CREATE TABLE forum(
+	forum_id INT IDENTITY(1,1)NOT NULL,
+	forum_title NVARCHAR(50)NOT NULL,
+	forum_description NVARCHAR(1000)NOT NULL,
+	image_path VARCHAR(200) NULL,
+	date_created DATETIME NOT NULL,
+	CONSTRAINT PK_forum PRIMARY KEY(forum_id)
+);
 
 CREATE TABLE replys(
-reply_id int IDENTITY(100001,1)NOT NULL,
-reply_content varchar(1000),
-up_votes INT NOT NULL,
-down_votes INT NOT NULL,
-date_created DATE NOT NULL
-CONSTRAINT PK_replys PRIMARY KEY(reply_id)
-)
+	reply_id int IDENTITY(1,1)NOT NULL,
+	reply_content NVARCHAR(MAX),
+	up_votes INT NOT NULL,
+	down_votes INT NOT NULL,
+	date_created DATETIME NOT NULL,
+	CONSTRAINT PK_replys PRIMARY KEY(reply_id)
+);
 
 --CREATE TABLE thread(
 --thread_id INT IDENTITY(10000,1)NOT NULL,
@@ -41,52 +51,52 @@ CONSTRAINT PK_replys PRIMARY KEY(reply_id)
 --CONSTRAINT PK_thread PRIMARY KEY(thread_id)
 --)
 
-CREATE TABLE forum(
-forum_id INT IDENTITY(1001,1)NOT NULL,
-forum_title VARCHAR(50)NOT NULL,
-forum_description VARCHAR(250)NOT NULL,
-image_path VARCHAR(200) NULL,
-date_created DATE NOT NULL,
-replys_in_forum INT NOT NULL
-CONSTRAINT PK_forum PRIMARY KEY(forum_id)
-)
-
 CREATE TABLE user_reply(
-user_id INT NOT NULL,
-reply_id INT NOT NULL
-CONSTRAINT [PK_user_reply] PRIMARY KEY(user_id, reply_id)
-FOREIGN KEY(user_id) REFERENCES [users] (user_id),
-FOREIGN KEY(reply_id) REFERENCES [replys] (reply_id)
-)
+	user_id INT NOT NULL,
+	reply_id INT NOT NULL,
+	CONSTRAINT [PK_user_reply] PRIMARY KEY(user_id, reply_id),
+	FOREIGN KEY(user_id) REFERENCES [users] (user_id),
+	FOREIGN KEY(reply_id) REFERENCES [replys] (reply_id)
+);
 
 CREATE TABLE reply_forum(
-reply_id INT NOT NULL,
-forum_id INT NOT NULL
-CONSTRAINT [PK_reply_thread] PRIMARY KEY(reply_id, forum_id)
-FOREIGN KEY(reply_id) REFERENCES [replys] (reply_id),
-FOREIGN KEY(forum_id) REFERENCES [forum] (forum_id)
-)
+	reply_id INT NOT NULL,
+	forum_id INT NOT NULL,
+	CONSTRAINT [PK_reply_thread] PRIMARY KEY(reply_id, forum_id),
+	FOREIGN KEY(reply_id) REFERENCES [replys] (reply_id),
+	FOREIGN KEY(forum_id) REFERENCES [forum] (forum_id)
+);
 
 
 --CREATE TABLE thread_forum(
 --thread_id INT NOT NULL,
---forum_id INT NOT NULL
+--forum_id INT NOT NULL,
 --CONSTRAINT [PK_thread_forum] PRIMARY KEY(thread_id, forum_id)
 --FOREIGN KEY(thread_id) REFERENCES [thread] (thread_id),
 --FOREIGN KEY(forum_id) REFERENCES [forum] (forum_id)
 --)
 
 CREATE TABLE user_forum(
-user_id INT NOT NULL,
-forum_id INT NOT NULL,
-is_moderator BIT NOT NULL
-CONSTRAINT [PK_user_forum] PRIMARY KEY (user_id, forum_id)
-FOREIGN KEY(user_id) REFERENCES [users] (user_id),
-FOREIGN KEY(forum_id) REFERENCES [forum] (forum_id)
-)
+	user_id INT NOT NULL,
+	forum_id INT NOT NULL,
+	is_moderator BIT NOT NULL,
+	CONSTRAINT [PK_user_forum] PRIMARY KEY (user_id, forum_id),
+	FOREIGN KEY(user_id) REFERENCES [users] (user_id),
+	FOREIGN KEY(forum_id) REFERENCES [forum] (forum_id)
+);
 
 CREATE TABLE favorited_forums(
-user_id INT NOT NULL,
-forum_id INT NOT NULL,
-favorited BIT NOT NULL
-)
+	user_id INT NOT NULL,
+	forum_id INT NOT NULL,
+	favorited BIT NOT NULL,
+	CONSTRAINT [PK_favorited_forums] PRIMARY KEY (user_id, forum_id),
+	FOREIGN KEY(user_id) REFERENCES [users] (user_id),
+	FOREIGN KEY(forum_id) REFERENCES [forum] (forum_id)
+);
+
+--populate default data
+--Inserting Users --
+INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
+INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
+
+-- Inserting
