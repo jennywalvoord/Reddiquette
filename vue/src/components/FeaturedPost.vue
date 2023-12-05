d43b01a3324817ad647513c11cf1bb5443af2d3b
Jenny Walvoord <jennywalvoord@gmail.com>
2023-12-05 16:22:59 -0500

    Worked on layout of home page and disabled navigation drawer

--- vue/src/components/FeaturedPost.vue
+++ vue/src/components/FeaturedPost.vue
@@ -1,20 +1,34 @@
 <template>
+    <div class="card pb-6">
+    <div class="well">
     <div>
-        <!--TOOD: fix alt text for images -->
-        <img src="">
-        <div>
-            <date></date>
-            <div class="vote">
-                <a href=""><i>upvote</i></a>
-                <p class="upvote">number</p>
-                <a href=""><i>downvote</i></a>
-            </div>
-            <div class="comment">
-                <a href=""><i>comment</i></a>
-                <p class="upvote">comment</p>
-            </div>
-        </div>
+      <img v-bind:src="post.image">
     </div>
+    <div>
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body.substring(0, 250) }}...</p>
+      <p>DATE POSTED: {{ post.dateCreated }}</p>
+      <p>CLOUT: {{ post.clout }}</p>
+    </div>
+  </div>
+
+  <div class="social">
+    <div class="vote">
+        <a @click="upVote">Upvote <i class="fa-solid fa-arrow-up"></i></a>
+        <!-- <p class="upvote">Upvote / Downvote</p> -->
+        <a @click="downVote">Downvote <i class="fa-solid fa-arrow-down"></i></a>
+    </div>
+    <div class="comment">
+        <a href=""><i class="fa-regular fa-comment"></i></a>
+        <p class="upvote">comment</p>
+    </div>
+    <div class="reply">
+        <a href=""><i class="fa-regular fa-comments"></i></a>
+        <p class="upvote">reply</p>
+    </div>
+</div>
+
+  </div>
 </template>
 
 <script>
