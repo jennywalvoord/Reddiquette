4eeb59abab78f1177f4c7da0926484bb59733931
Jenny Walvoord <jennywalvoord@gmail.com>
2023-12-05 15:50:30 -0500

    more formatting done

--- vue/src/components/SmallPost.vue
+++ vue/src/components/SmallPost.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="well">
+  <div class="card pb-6">
+    <div class="well">
     <div>
       <img v-bind:src="post.image">
     </div>
@@ -8,19 +9,27 @@
       <p>{{ post.body.substring(0, 250) }}...</p>
       <p>DATE POSTED: {{ post.dateCreated }}</p>
       <p>CLOUT: {{ post.clout }}</p>
-      <div>
-        <div class="vote">
-      <a @click="upVote">Upvote</a>
-      <p class="clout">{{ $store.clout }}</p>
-      <a @click="downVote">Downvote</a>
-    </div>
-        <div class="comment">
-          <a href=""><i>comment</i></a>
-          <p class="upvote">comment</p>
-        </div>
-      </div>
     </div>
   </div>
+
+  <div class="social">
+    <div class="vote">
+        <a @click="upVote">Upvote <i class="fa-solid fa-arrow-up"></i></a>
+        <!-- <p class="upvote">Upvote / Downvote</p> -->
+        <a @click="downVote">Downvote <i class="fa-solid fa-arrow-down"></i></a>
+    </div>
+    <div class="comment">
+        <a href=""><i class="fa-regular fa-comment"></i></a>
+        <p class="upvote">comment</p>
+    </div>
+    <div class="reply">
+        <a href=""><i class="fa-regular fa-comments"></i></a>
+        <p class="upvote">reply</p>
+    </div>
+</div>
+
+  </div>
+  
 </template>
 
 <script>
@@ -39,3 +48,51 @@ export default {
   },
 };
 </script>
+
+<style>
+.card {
+  max-width: 600px;
+ 
+
+}
+img {
+  border-radius: 8px;
+  height: 200px;
+}
+.well {
+    display: flex;
+    gap: 24px;
+}
+.social {
+    display: flex;
+    align-items: center;
+}
+.vote,
+.comment,
+.reply {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 10px 20px;
+}
+.arrow {
+    font-size: 20px;
+    margin-right: 5px;
+}
+.comment-icon {
+    font-size: 20px;
+    margin-right: 5px;
+}
+.vote a,
+.comment a,
+.reply a {
+    text-decoration: none;
+    color: #333;
+}
+.vote p,
+.comment p,
+.reply p {
+    margin: 0;
+    padding: 0;
+}
+</style>
