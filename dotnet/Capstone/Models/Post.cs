b32f0f00118201dfc7880297422e3ef34ad70050
John.Detwiller <john.t.detwiller@gmail.com>
2023-12-06 14:57:07 -0500

    I think the api is done, so far...

--- dotnet/Capstone/Models/Post.cs
+++ dotnet/Capstone/Models/Post.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Capstone.Models
 {
     public class Post
     {
         public int PostID { get; set; }
+
+        [Required(ErrorMessage = "Post title is required.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Post content is required.")]
         public string Content { get; set; }
+
         public int UpVotes { get; set; }
+
         public int DownVotes { get; set; }
+
+        [Required(ErrorMessage = "Date created is required.")]
+        public DateTime DatePosted { get; set; }
+
+        public string ImagePath { get; set; }
+
         //TODO: complete Post Model
     }
 }
