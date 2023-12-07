bdb52b1c2d7a18a4a299152ac1eff3d1713fbdb1
Wayne Wallace <flikken84@gmail.com>
2023-12-07 14:06:23 -0500

    trying to get sorting posts working

--- vue/src/components/ForumSection.vue
+++ vue/src/components/ForumSection.vue
@@ -1,39 +1,46 @@
 <template>
-<v-content>
-  <div>
-        <v-container class="my-5">
+  <v-content>
+    <div>
+      <v-container class="my-5">
         <v-layout row>
           <v-flex xs12 md6 lg1>
-            <small-post v-for="post in filteredPosts" v-bind:key="post.id" v-bind:post="post" />
+            <small-post
+              v-for="post in sortedPosts"
+              :key="post.id"
+              :post="post"
+            />
           </v-flex>
         </v-layout>
       </v-container>
     </div>
-
-</v-content>
-
-    
+  </v-content>
 </template>
 
 <script>
-import { storeKey } from 'vuex';
 import SmallPost from '../components/SmallPost.vue';
 
 export default {
-  props: ["forum"],
+  props: ["forum","sortBy"],
   components: {
     SmallPost,
   },
   computed: {
-    filteredPosts() {
+    sortedPosts() {
+      if (!this.forum || !this.forum.id) {
+        return [];
+      }
+
       let forumId = this.forum.id;
-    let posts = this.$store.state.posts.filter((p) => p.forumId === forumId);
+      let posts = this.$store.state.posts.filter((p) => p.forumId === forumId);
 
-    posts.sort((a, b) => new Date(b.dateCreated) - new Date(a.dateCreated));
-
-    return posts;
-    }
-  }
-}
-
+      if (this.sortBy === 'date') {
+        posts.sort((a, b) => new Date(b.dateCreated) - new Date(a.dateCreated));
+      } else if (this.sortBy === 'clout') {
+        posts.sort((a, b) => b.clout - a.clout);
+      } 
+      return posts;
+    },
+  },
+  
+};
 </script>
