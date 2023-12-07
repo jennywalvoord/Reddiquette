4e7b8a5b450ecec5a0263dde3e8622bb774834aa
Jenny Walvoord <jennywalvoord@gmail.com>
2023-12-07 14:05:02 -0500

    working on ForumView...

--- vue/src/views/ForumView.vue
+++ vue/src/views/ForumView.vue
@@ -1,34 +1,30 @@
 <template>
-    
-    <v-sheet class="d-flex flex-wrap bg-surface-variant">
-        <v-sheet class="flex-1-0 ma-24 pa-24">
-            <forum-section v-bind:forum="findForum" />
-        </v-sheet>
-        <v-sheet class="ma-2 pa-2">
-            <side-bar v-bind:forums="forums" />
-        </v-sheet>
-        <v-sheet class="flex-1-1-100 ma-2 pa-2">
-        </v-sheet>
-    </v-sheet>
-</template>
-
-<!-- <template>
-<v-content >
     <div class="d-flex flex-wrap grey-lighten pa-24">
-    <div class="flex-1-0 ma-24 pa-24">
-      <forum-section />
+    <div class="ma-24 pa-24">
+      <div class="filter-bar-wrapper">
+        <filter-bar />
+      </div>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="12">
+            <v-row>
+              <forum-section v-bind:forum="findForum" />
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
 
-</div>
     <v-navigation-drawer fixed permanent location="right">
-      <div class="flex-1-0 ma-24 pa-24">
-        <side-bar v-bind:forums="forums"/>
+      <div class="ma-24 pa-24">
+        <side-bar v-bind:forums="forums" />
       </div>
     </v-navigation-drawer>
-</div>
+  </div>
+  
+</template>
 
-</v-content>
 
-</template> -->
 
 <script>
 import ForumSection from '../components/ForumSection.vue';
