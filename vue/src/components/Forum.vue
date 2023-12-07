48c1133a29a54a85777355502016bbf366a2c03b
Wayne Wallace <flikken84@gmail.com>
2023-12-07 14:07:49 -0500

    Stuff maybe

--- vue/src/components/Forum.vue
+++ vue/src/components/Forum.vue
@@ -1,27 +1,36 @@
 <template>
-<v-content>
-    <div class="well">
+    <v-content>
+      <div class="well">
         <div>
-            
+          <!-- Your forum component content here -->
         </div>
         <div>
-            <h3>
-                <router-link  v-bind:to="{ name: 'forum-view', params: {id: forums.id}}">{{forums.title}}</router-link>
-            </h3>
-            
-        
+          <h3>
+            <router-link
+              v-bind:to="{ name: 'forum-view', params: { id: forum.id }}"
+            >
+              {{ forum.title }}
+            </router-link>
+          </h3>
+          <!-- Pass sortBy prop dynamically based on user interaction -->
+          <forum-section :forum="forum" :sortBy="sortBy" />
         </div>
-    </div>
-
-</v-content>
-
-    
-</template>
-<script>
-
-export default {
-    props: ['forums']
-   
-}
-
-</script>
+      </div>
+    </v-content>
+  </template>
+  
+  <script>
+  import ForumSection from './ForumSection.vue';
+  
+  export default {
+    props: ['forum'],
+    components: {
+      ForumSection,
+    },
+    data() {
+      return {
+        sortBy: 'date', // or 'clout'
+      };
+    },
+  };
+  </script>
