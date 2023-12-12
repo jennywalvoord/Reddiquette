0309c41528ae51a169d8c0a3bb175674d45e194b
Khalil Bonilla <khabotech@gmail.com>
2023-12-12 09:58:50 -0500

    Search bar and typo

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <v-app class="grey lighten-4">
-    <!-- <navbar></navbar> -->
-    <!-- <div style="height: 72px;"></div> -->
-   
-      <v-app-bar app
-      class="px-3 elevation-4"
-      color="grey-lighten-4"
-      height="72"
-      > 
-        <v-app-bar-nav-icon>
-            <router-link to="/">
-              <v-avatar size="48">
-                <v-img
-                src="./src/assets/Logo_Orange.png"
-                width="100"
-                height="100"
-            />
-              </v-avatar>
-             
-            </router-link>
-          </v-app-bar-nav-icon>
-          <v-app-bar-title class="font-weight-bold" :style="{color: '#ff4500'}">rediquette</v-app-bar-title>
-          <router-link v-bind:to="{ name: 'home' }">Home</router-link>
-          <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/81.jpg">
+    <v-app-bar app class="px-3 elevation-4" color="grey-lighten-4" height="72">
+      <v-app-bar-nav-icon>
+        <router-link to="/">
+          <v-avatar size="48">
+            <v-img src="./src/assets/Logo_Orange.png" width="100" height="100" />
+          </v-avatar>
+        </router-link>
+      </v-app-bar-nav-icon>
+      <v-app-bar-title class="font-weight-bold" :style="{ color: '#ff4500' }">ReddiQuette</v-app-bar-title>
+      
+      <!-- Centered search bar -->
+      <v-row align="center" class="mx-auto">
+        <v-col cols="12">
+          <v-text-field
+            v-model="search"
+            placeholder="Search ReddiQuette"
+            append-icon="mdi-magnify"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      
+      <router-link v-bind:to="{ name: 'home' }">Home</router-link>
+      
+      <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/81.jpg">
         <v-list-item-content>
           <v-list-item-title>{{ displayedUsername }}</v-list-item-title>
           <v-list-item-subtitle>
@@ -31,13 +34,9 @@
           </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
-          
-    </v-app-bar> 
-
+    </v-app-bar>
 
     <router-view />
-
-
   </v-app>
 </template>
 
@@ -77,6 +76,10 @@ export default {
 
 div.nav{
   flex-flow: row;
+}
+
+input{
+  align-items: center;
 }
 /* .global-padding {
   padding: 80px;
