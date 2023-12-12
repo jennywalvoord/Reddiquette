d4bb55a7990a4b3c4e953824116d3dd77bf47db8
Veroh1 <Richard.M.Reske@Gmail.com>
2023-12-12 12:13:11 -0500

    Vote API path

--- dotnet/Capstone/DAO/IVotesDao.cs
+++ dotnet/Capstone/DAO/IVotesDao.cs
@@ -5,7 +5,9 @@ namespace Capstone.DAO
     public interface IVotesDao
     {
         ReturnVotes GetPostVotesById(int targetID);
-        ReturnVotes GetCommentVoteById(int targetID);
+        Vote GetPostVoteById(int userId, int postId);
+        ReturnVotes GetCommentVotesById(int targetID);
+        Vote GetCommentVoteById(int userId, int commentId);
         Vote CreatePostVote(int userId, int targetID, int increment);
         Vote CreateCommentVote(int userId, int targetID, int increment);
     }
--- dotnet/Capstone/DAO/VotesDao.cs
+++ dotnet/Capstone/DAO/VotesDao.cs
@@ -1,4 +1,5 @@
-﻿using Capstone.Models;
+﻿using Capstone.Exceptions;
+using Capstone.Models;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
@@ -17,12 +18,11 @@ namespace Capstone.DAO
 
         public ReturnVotes GetPostVotesById(int targetID)
         {
-            ReturnVotes votes = null;
+            ReturnVotes votes = new ReturnVotes() { TargetID = targetID, Upvotes = 0, Downvotes = 0 };
             IList<Vote> votesList = new List<Vote>();
-            int upCount = 0;
-            int downCount = 0;
+           
 
-            string query = "SELECT * FROM post_votes WHERE post_id = @postId";
+            string query = "SELECT * FROM post_votes WHERE target_id = @postId";
 
             try
             {
@@ -32,11 +32,209 @@ namespace Capstone.DAO
                     SqlCommand cmd = new SqlCommand(query, conn);
                     cmd.Parameters.AddWithValue("postId", targetID);
                     SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Vote vote = MapRowToVote(reader);
+                        votesList.Add(vote);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new DaoException("SQL exception occurred", ex);
+            }
+            foreach (Vote vote in votesList)
+            {
+                if (vote.Increment == 1)
+                {
+                    votes.Upvotes += 1;
+                }
+                if(vote.Increment == -1)
+                {
+                    votes.Downvotes += 1;
                 }
             }
 
+            return votes;
+        }
+
+        public Vote GetPostVoteById(int userId, int postId)
+        {
+            Vote vote = null;
+            string sql = "SELECT * FROM post_votes WHERE user_id = @userId AND target_id = @targetId";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@userId", userId);
+                    cmd.Parameters.AddWithValue("@targetId", postId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        vote = MapRowToVote(reader);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new DaoException("SQL exception occurred", ex);
+            }
+
+            return vote;
+        }
+
+        public ReturnVotes GetCommentVotesById(int targetID)
+        {
+            ReturnVotes votes = new ReturnVotes() { TargetID = targetID, Upvotes = 0, Downvotes = 0 };
+            IList<Vote> votesList = new List<Vote>();
+         
+            string query = "SELECT * FROM comment_votes WHERE target_id = @commentId";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("commentId", targetID);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Vote vote = MapRowToVote(reader);
+                        votesList.Add(vote);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new DaoException("SQL exception occurred", ex);
+            }
+            foreach (Vote vote in votesList)
+            {
+                if (vote.Increment == 1)
+                {
+                    votes.Upvotes += 1;
+                }
+                if (vote.Increment == -1)
+                {
+                    votes.Downvotes += 1;
+                }
+            }
 
             return votes;
+        }
+
+        public Vote GetCommentVoteById(int userId, int commentId)
+        {
+            Vote vote = null;
+            string sql = "SELECT * FROM comment_votes WHERE user_id = @userId AND target_id = @targetId";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@userId", userId);
+                    cmd.Parameters.AddWithValue("@targetId", commentId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        vote = MapRowToVote(reader);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new DaoException("SQL exception occurred", ex);
+            }
+
+            return vote;
+        }
+
+        public Vote CreatePostVote(int userId, int targetID, int increment)
+        {
+            Vote vote = null;
+
+            string sql = "INSERT INTO post_votes(user_id, target_id, inc) " +
+                "VALUES (@userId, @targetId, @inc)";
+            string sql2 = "SELECT * FROM post_votes WHERE user_id = @userId AND target_id = @targetId";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@userId", userId);
+                    cmd.Parameters.AddWithValue("@targetId", targetID);
+                    cmd.Parameters.AddWithValue("@inc", increment);
+
+                    cmd.ExecuteNonQuery();
+
+                    SqlCommand cmd2 = new SqlCommand(sql2, conn);
+                    cmd2.Parameters.AddWithValue("@userId", userId);
+                    cmd2.Parameters.AddWithValue("@targetId", targetID);
+                    SqlDataReader reader = cmd2.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        vote = MapRowToVote(reader);
+                    }
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new DaoException("SQL exception occurred", ex);
+            }
+            return vote;
+        }
+
+        public Vote CreateCommentVote(int userId, int targetID, int increment)
+        {
+            Vote vote = null;
+
+            string sql = "INSERT INTO comment_votes(user_id, target_id, inc) " +
+                "VALUES (@userId, @targetId, @inc)";
+            string sql2 = "SELECT * FROM comment_votes WHERE user_id = @userId AND target_id = @targetId";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@userId", userId);
+                    cmd.Parameters.AddWithValue("@targetId", targetID);
+                    cmd.Parameters.AddWithValue("@inc", increment);
+
+                    cmd.ExecuteNonQuery();
+
+                    SqlCommand cmd2 = new SqlCommand(sql2, conn);
+                    cmd2.Parameters.AddWithValue("@userId", userId);
+                    cmd2.Parameters.AddWithValue("@targetId", targetID);
+                    SqlDataReader reader = cmd2.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        vote = MapRowToVote(reader);
+                    }
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new DaoException("SQL exception occurred", ex);
+            }
+            return vote;
         }
         private Vote MapRowToVote(SqlDataReader reader)
         {
